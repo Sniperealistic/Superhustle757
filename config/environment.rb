4e4e8f273711d38be0519edc60eebc6abd0509c4
saasbook <saasbook@saasbook.(none)>
2013-10-22 16:28:42 -0400

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+SuperHustle757::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperHustle757::Application.config.secret_token = '3e7c441dff6a610528761a6d2958c96ed3fd15783c4b2df2a2af773310c0f83561e67caab825dffdf43717c9748a7e4758a0a9939b4a83c92e247b56e42ed29f'
