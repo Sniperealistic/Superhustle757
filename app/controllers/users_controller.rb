4429b72fad1fa50f41b4cfadf75cddea817135fc
Sniperealistic <mr.cloud10@gmail.com>
2013-11-19 18:32:04 -0500

    new file:   app/assets/javascripts/sessions.js.coffee 	new file:   app/assets/javascripts/users.js.coffee 	new file:   app/assets/stylesheets/sessions.css.scss 	new file:   app/assets/stylesheets/users.css.scss 	modified:   app/controllers/application_controller.rb 	new file:   app/controllers/sessions_controller.rb 	new file:   app/controllers/sessions_controller.rb~ 	new file:   app/controllers/users_controller.rb 	new file:   app/controllers/users_controller.rb~ 	new file:   app/helpers/sessions_helper.rb 	new file:   app/helpers/users_helper.rb 	new file:   app/models/user.rb 	new file:   app/models/user.rb~ 	new file:   app/views/sessions/new.html.erb 	new file:   app/views/sessions/new.html.erb~ 	new file:   app/views/users/index.html.erb 	new file:   app/views/users/index.html.erb~ 	new file:   app/views/users/new.html.erb 	new file:   app/views/users/new.html.erb~ 	new file:   app/views/users/show.html.erb 	modified:   config/routes.rb 	new file:   db/development.sqlite3 	new file:   db/migrate/20131119213221_create_users.rb 	modified:   db/schema.rb 	new file:   db/test.sqlite3 	new file:   log/development.log 	new file:   tmp/cache/assets/C1B/350/sprockets%2F428286af6252d5193372732c81410a6e 	new file:   tmp/cache/assets/CB0/D60/sprockets%2F0335d87ebd93523744c2fec4426f4432 	new file:   tmp/cache/assets/CD8/370/sprockets%2F357970feca3ac29060c1e3861e2c0953 	new file:   tmp/cache/assets/D02/0A0/sprockets%2F4ad413f3e3326481d2f5f9327bbd0e12 	new file:   tmp/cache/assets/D03/670/sprockets%2F6c0f2a2c7346b84a92e2cb81473e251b 	new file:   tmp/cache/assets/D0D/7B0/sprockets%2Fb47c99405d57a13c185d6db26ff350c1 	new file:   tmp/cache/assets/D32/A10/sprockets%2F13fe41fee1fe35b49d145bcc06610705 	new file:   tmp/cache/assets/D3A/BE0/sprockets%2F73d5b3d4a4584cbe9dfa3937b134c007 	new file:   tmp/cache/assets/D4E/1B0/sprockets%2Ff7cbd26ba1d28d48de824f0e94586655 	new file:   tmp/cache/assets/D5A/EA0/sprockets%2Fd771ace226fc8215a3572e0aa35bb0d6 	new file:   tmp/cache/assets/D66/E60/sprockets%2F85ae64edf528ce6e981889f3f3c285d9 	new file:   tmp/cache/assets/D6A/D20/sprockets%2F4d59271fb0a82ea65274fcf1f1b14db9 	new file:   tmp/cache/assets/DA4/460/sprockets%2F977ead1d20bc9f7e1c191bd4348f9e3a 	new file:   tmp/cache/assets/DDC/400/sprockets%2Fcffd775d018f68ce5dba1ee0d951a994 	new file:   tmp/cache/assets/DEE/1C0/sprockets%2F9192ecdab77bb825e20d50d64ecaa0da 	new file:   tmp/cache/assets/DF1/710/sprockets%2F0f8aa08bcb16235a9f63bb55b7cdaad8 	new file:   tmp/cache/assets/DFC/470/sprockets%2F8a0e7cc1ca5e1af0b1a30ac42ea00e1a 	new file:   tmp/cache/assets/E04/890/sprockets%2F2f5173deea6c795b8fdde723bb4b63af 	new file:   tmp/cache/sass/f06ab9a2bc74374d8129c22f3009b4fc632dd04a/sessions.css.scssc 	new file:   tmp/cache/sass/f06ab9a2bc74374d8129c22f3009b4fc632dd04a/users.css.scssc 	modified:   tmp/pids/server.pid

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_filter :check_login, :only => [:index]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      flash[:status] = TRUE
+      flash[:alert] = 'Congratulations on registering at SUPERHUSTLE757'
+    else
+      flash[:status] = FALSE
+      flash[:alert] = @user.errors.full_messages
+    end
+
+    redirect_to register_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SuperHustle757::Application.routes.draw do
+  resources :users
+  match '/register' => 'users#new'
 
-  resources :customers
-  root :to =>redirect("/customers")
+  resources :sessions, :only => [:new, :create, :destroy]
+  match 'login' => 'sessions#new'
+  match 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +55,7 @@ SuperHustle757::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
