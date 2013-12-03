8558e50e6d87b2485a6a0a0168b983ff495aa2bb
Sniperealistic <mr.cloud10@gmail.com>
2013-12-02 16:15:37 -0800

    I have changed the title page so that it has links to the register and log in page. As well as put links on the register page to lead to the login and vice versa.
    	modified:   app/controllers/page_controller.rb
    	modified:   app/views/page/index.html.erb
    	modified:   app/views/sessions/new.html.erb
    	modified:   app/views/users/index.html.erb
    	modified:   app/views/users/new.html.erb
    	modified:   app/views/users/show.html.erb
    	modified:   log/development.log
    	modified:   tmp/cache/assets/CD8/370/sprockets%2F357970feca3ac29060c1e3861e2c0953
    	modified:   tmp/cache/assets/D32/A10/sprockets%2F13fe41fee1fe35b49d145bcc06610705
    	modified:   tmp/cache/assets/D4E/1B0/sprockets%2Ff7cbd26ba1d28d48de824f0e94586655
    	modified:   tmp/cache/assets/D5A/EA0/sprockets%2Fd771ace226fc8215a3572e0aa35bb0d6
    	modified:   tmp/cache/assets/DDC/400/sprockets%2Fcffd775d018f68ce5dba1ee0d951a994
    	modified:   tmp/cache/assets/E04/890/sprockets%2F2f5173deea6c795b8fdde723bb4b63af
    	modified:   tmp/pids/server.pid

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,17 +1,25 @@
 class PageController < ApplicationController
-def index
-end
-def show
-@page = Page.find(params[id])
-end
-def new
-end 
-def create 
-end 
-def edit 
-end 
-def update
-end 
-def destroy
-end 
+  before_filter :check_login, :only => [:index]
+
+  def index
+  end
+
+  def show
+    @page = Page.find(params[id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
